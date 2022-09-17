e3e4aef8c1d9f5af850820a4a2df3289dfb786c7
Diego Sano <diego.sano@nave.rs>
2022-09-17 16:15:07 -0300

    feat: get notifications

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,4 +1,7 @@
+import * as Notifications from 'expo-notifications';
+import { useRef, useEffect } from 'react';
 import { StatusBar } from 'react-native';
+import { Subscription } from 'expo-modules-core';
 import {
   useFonts,
   Inter_400Regular,
@@ -11,7 +14,44 @@ import { Background } from './src/components/Background';
 import { Loader } from './src/components/Loader';
 import { Routes } from './src/routes';
 
+import { getPushNotificationToken } from './src/services/getPushNotificationToken';
+
+import './src/services/notificationConfig';
+
 export default function App() {
+  const getNotificationListener = useRef<Subscription>();
+  const responseNotificationListener = useRef<Subscription>();
+
+  useEffect(() => {
+    getPushNotificationToken();
+  }, []);
+
+  useEffect(() => {
+    getNotificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) =>
+        console.log(notification)
+      );
+
+    responseNotificationListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) =>
+        console.log(response)
+      );
+
+    return () => {
+      if (getNotificationListener.current) {
+        Notifications.removeNotificationSubscription(
+          getNotificationListener.current
+        );
+      }
+
+      if (responseNotificationListener.current) {
+        Notifications.removeNotificationSubscription(
+          responseNotificationListener.current
+        );
+      }
+    };
+  }, []);
+
   const [isFontLoaded] = useFonts({
     Inter_400Regular,
     Inter_600SemiBold,
---
+++ mobile/src/services/getPushNotificationToken.ts
@@ -0,0 +1,16 @@
+import * as Notifications from 'expo-notifications';
+
+export async function getPushNotificationToken() {
+  const { granted } = await Notifications.getPermissionsAsync();
+
+  if (!granted) {
+    await Notifications.requestPermissionsAsync();
+  }
+
+  if (granted) {
+    const token = await Notifications.getExpoPushTokenAsync();
+    console.log('NOTIFICATION TOKEN:', token);
+
+    return token.data;
+  }
+}
